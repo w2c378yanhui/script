59c24bfefbc5a99f43729d03ca8b3f18eba04e67
king <qwer1234>
2017-04-12 16:21:36 +0800

    rm outter file

--- anr.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /Log/anr/.*
-adb pull /data/anr /Users/king/Log
-open -e /Users/king/Log/anr/*.txt
-adb shell rm /data/anr/*
--- jietu.sh
+++
@@ -1,5 +0,0 @@
-rm -rf $HOME/Downloads/yanhui*.png
-Time=$(date +%y-%m-%d:%H:%M:%S)
-adb shell /system/bin/screencap -p /sdcard/yanhui$Time.png
-adb pull /sdcard/yanhui$Time.png $HOME/Downloads
-open $HOME/Downloads/yanhui$Time.png
